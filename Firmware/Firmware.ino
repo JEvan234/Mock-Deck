c23444cabc19d0a89b1f058327781d0b233770c7
Zane S. <jevan234@students.kennesaw.edu>
2025-03-12 22:38:52 -0400

    Added documentation to the INO file

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,12 +1,30 @@
+/* Created by JEvan234
+Part of the Mock-Deck Project on Github
+https://github.com/JEvan234/Mock-Deck
+
+This File serves as the INO file, or "firmware" that will be flashed to your pico.
+In my case I am using a Pico 2.
+
+3/12/2025 - As of right now all pins are according to my setup as I work things out
+*/
+
+// Header Files:
 #include <Mouse.h>
 
+
+// States and declared pins. Pins are constant and are declared as such
+// Pins 1 and 2 on Pico
 const int downPin = 0;
 const int upPin = 1;
+// Buttonstates help read High and Low, and can change
 int buttonState0 = 0;
 int buttonState1 = 0;
 
+
 void setup() {
+  // Establishes a serial connection using a "115200" Baud rate, which is default for the Pico 2
   Serial.begin(115200);
+  // Establishes pins as inputs
   pinMode(downPin, INPUT_PULLDOWN);
   pinMode(upPin, INPUT_PULLDOWN);
   Mouse.begin();
@@ -14,6 +32,7 @@ void setup() {
 }
 
 void loop() {
+  // Reads States abd performs actions accordingly
   buttonState0 = digitalRead(downPin);
   buttonState1 = digitalRead(upPin);
   if (buttonState0 == HIGH) {
